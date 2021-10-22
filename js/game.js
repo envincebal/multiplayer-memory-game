1b107c45b2bbb283fc1779ff3531de92a385f9bd
envincebal <vincentyan1@gmail.com>
2021-10-22 03:26:05 -0700

    Added addPlayer function.

--- js/game.js
+++ js/game.js
@@ -1,35 +1,78 @@
 import {
   iconsArray,
-  numArray
+  numArray,
+  playersArray
 } from "./themeArrays.js";
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
 let timer; // Serves as reference for time interval.
-
-
 const cardButton = document.querySelectorAll(".card-button");
-const menuSettings = JSON.parse(localStorage.getItem("menu"));
-const themeSetting = menuSettings.theme === "icons" ? iconsArray : numArray;
-const random =  shuffle(themeSetting); // Stores shuffle function with array of card as argument.
+const theme = localStorage.getItem("theme");
+const players = localStorage.getItem("players");
+const grid = localStorage.getItem("grid");
+
+let numMode = grid === "4x4" ? numArray.slice(0, 16) : numArray;
+let iconsMode = grid === "4x4" ? iconsArray.slice(0, 16) : iconsArray;
+let themeSetting = theme === "icons" ? iconsMode : numMode;
+const random = shuffle(themeSetting); // Stores shuffle function with array of card as argument.
 
 init();
 
 function init() {
+  const card = document.querySelectorAll(".card");
+  const player = document.querySelectorAll(".player");
 
 
-
-  if (menuSettings.theme === "numbers") {
-    for (let i = 0; i < numArray.length; i++) {
-      cardButton[i].append(random[i]);
-
-    }
-  }else{
-    for (let i = 0; i < iconsArray.length; i++) {
-      cardButton[i].innerHTML = random[i];
-    }
+  if (themeSetting === "numbers") {
+    cardButton.forEach((num, i) => {
+      num.append(random[i])
+    })
+  } else {
+    cardButton.forEach((icon, i) => {
+      icon.innerHTML = random[i];
+    })
   }
 
-startTimer();
+  for (let i = 1; i <= players; i++) {
+    addPlayer([i]);
+  }
+
+
+
+  if (grid === "4x4") {
+    Array.from(card).forEach(item => {
+      if (item.classList.contains("hard-card")) {
+        item.style.display = "none";
+      }
+    })
+
+    Array.from(cardButton).forEach(item => {
+      item.style.width = "118px";
+      item.style.height = "118px";
+    });
+  }
+}
+
+function addPlayer(num) {
+  const timer = document.querySelector(".time-div");
+  const playerList = document.querySelector(".players-list");
+  const listItem = document.createElement("li");
+  const playerHeader = document.createElement("h3");
+  const playerMoves = document.createElement("p");
+
+  playerHeader.innerText = "Player " + num;
+  playerMoves.innerText = 0;
+  listItem.classList.add("moves-div");
+  listItem.classList.add("player");
+  playerMoves.classList.add("moves");
+  playerList.appendChild(listItem);
+  listItem.appendChild(playerHeader);
+  listItem.appendChild(playerMoves);
+
+  if (players > 1) {
+    timer.style.display = "none";
+  }
+
 }
 
 function shuffle(array) {
--- js/menu.js
+++ js/menu.js
@@ -1,57 +1,61 @@
-const menuSettings = JSON.parse(localStorage.getItem("menu"));
+const theme = document.querySelectorAll(".btn-theme");
+const playerNum = document.querySelectorAll(".btn-players");
+const grid = document.querySelectorAll(".btn-grid");
 
-menu();
+localStorage.setItem("theme", "numbers");
+localStorage.setItem("players", "1");
+localStorage.setItem("grid", "4x4");
 
-function menu() {
-  const theme = document.querySelectorAll(".btn-theme");
-  const playerNum = document.querySelectorAll(".btn-players");
-  const grid = document.querySelectorAll(".btn-grid");
+Array.from(theme).forEach(el => {
+  el.addEventListener("click", (e) => {
+    const selected = document.getElementsByClassName("theme-active");
+    selected[0].className = selected[0].className.replace(" theme-active", "");
+    el.className += " theme-active";
 
-  localStorage.setItem("menu", JSON.stringify({
-    "theme": "numbers",
-    "players": ""
-  }));
-
-  Array.from(theme).forEach(el => {
-    el.addEventListener("click", (e) => {
-      const selected = document.getElementsByClassName("theme-active");
-      selected[0].className = selected[0].className.replace(" theme-active", "");
-      el.className += " theme-active";
-      if (e.target.value === "icons") {
-
-        localStorage.setItem("menu", JSON.stringify({
-          "theme": "icons"
-        }));
-
-      }
-
-
-    })
-  });
+    if (e.target.value === "icons") {
+      localStorage.setItem("theme", "icons");
+    }
+    if (e.target.value === "numbers") {
+      localStorage.setItem("theme", "numbers");
+    }
+  })
+});
 
 
 
-  Array.from(playerNum).forEach(el => {
-    el.addEventListener("click", (e) => {
-      const selected = document.getElementsByClassName("players-active");
-      selected[0].className = selected[0].className.replace(" players-active", "");
-      el.className += " players-active";
-      console.log(e.target)
+Array.from(playerNum).forEach(el => {
+  el.addEventListener("click", (e) => {
+    const selected = document.getElementsByClassName("players-active");
+    selected[0].className = selected[0].className.replace(" players-active", "");
+    el.className += " players-active";
 
-      switch (e.target.value) {
-        case 1:
-      }
-    })
-  });
+    switch (e.target.value) {
+      case "1":
+        localStorage.setItem("players", "1");
+        break;
+      case "2":
+        localStorage.setItem("players", "2");
+        break;
+      case "3":
+        localStorage.setItem("players", "3");
+        break;
+      case "4":
+        localStorage.setItem("players", "4");
+        break;
+    }
+  })
+});
 
-  Array.from(grid).forEach(el => {
-    el.addEventListener("click", (e) => {
-      const selected = document.getElementsByClassName("grid-active");
-      selected[0].className = selected[0].className.replace(" grid-active", "");
-      el.className += " grid-active";
-    })
-  });
+Array.from(grid).forEach(el => {
+  el.addEventListener("click", (e) => {
+    const selected = document.getElementsByClassName("grid-active");
+    selected[0].className = selected[0].className.replace(" grid-active", "");
+    el.className += " grid-active";
 
-
-
-}
+    if (e.target.value === "6x6") {
+      localStorage.setItem("grid", "6x6");
+    } else {
+      localStorage.setItem("grid", "4x4");
+    }
+  })
+});
